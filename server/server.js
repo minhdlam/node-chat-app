2384e624ca760c230a1a2ef59e62ed023d412f4b
minhdlam <minhdlam@gmail.com>
2017-01-02 15:47:07 -0600

    Add connect and disconnect handlers using socketio

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000; //process.env.port is for heroku(3000 is for local)
 
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection', (socket) => { //an event listener (listening for a client to connect)
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('This user has disconnected');
+  });
+});
+
 app.use(express.static(publicPath));
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
